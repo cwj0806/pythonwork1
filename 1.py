2c0c8c1d0f472a258270f1c3944ca41046f6414b
cwj0806 <101384129+cwj0806@users.noreply.github.com>
2022-05-05 22:55:37 +0900

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,2 +1,21 @@
 #python algorithm
 #doubly linked list
+class tools:
+    def go(self,e):
+        addList.append(e)
+
+    def forward(self):
+        return
+
+    def backward(self):
+        return
+
+    def history(self):
+        return
+
+    def quit(self):
+        return
+
+
+addList=[]
+hisadrList=[]
